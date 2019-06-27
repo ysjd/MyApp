0af7685c2752bedba7d5b394d196cd304af430db
jyh <jyhryn@163.com>
2019-06-27 15:40:41 +0800

    回退到测试类1

---
+++ app/src/main/java/com/jyh/myapp/GitTest2.java
@@ -0,0 +1,11 @@
+package com.jyh.myapp;
+
+import android.util.Log;
+
+//git测试2
+public class GitTest2 {
+    private final String TAG = "GitTest2";
+    public void test1(){
+        Log.i(TAG,"dev分支  修改内容");
+    };
+}
