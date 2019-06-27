df1ab3d5d6f6a3e5a559c8917c198cda6a7a62dc
jyh <jyhryn@163.com>
2019-06-27 14:53:48 +0800

    添加测试类1

---
+++ app/src/main/java/com/jyh/myapp/GitTest1.java
@@ -0,0 +1,11 @@
+package com.jyh.myapp;
+
+import android.util.Log;
+
+//git测试1
+public class GitTest1 {
+    private final String TAG = "GitTest1";
+    public void test1(){
+        Log.i(TAG,"");
+    };
+}
