324408ed772380af3e8200c9c1395c03b7341a5e
jyh <jyhryn@163.com>
2019-06-27 14:44:44 +0800

    第一次提交

---
+++ app/src/main/java/com/jyh/myapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jyh.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
